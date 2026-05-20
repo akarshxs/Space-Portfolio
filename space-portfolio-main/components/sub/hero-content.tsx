3a7f90384f4e6badb50703937006fd0b1933e2e7
Akarsh Tripathi <xoxoakarsh@gmail.com>
2026-05-20 11:11:03 +0530

    Update hero-content.tsx

--- space-portfolio-main/components/sub/hero-content.tsx
+++ space-portfolio-main/components/sub/hero-content.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Image from "next/image";
-
 import {
   slideInFromLeft,
   slideInFromRight,
@@ -21,7 +19,7 @@ export const HeroContent = () => {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
@@ -31,14 +29,13 @@ export const HeroContent = () => {
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing{" "}
+            Hey, I'm{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              the best
-            </span>{" "}
-            project experience.
+              Akarsh Tripathi
+            </span>
           </span>
         </motion.div>
 
