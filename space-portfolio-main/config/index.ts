7f8e389c16038e6dddcb3610f50d30a20471402d
Akarsh Tripathi <xoxoakarsh@gmail.com>
2026-05-20 11:33:48 +0530

    Update index.ts

--- space-portfolio-main/config/index.ts
+++ space-portfolio-main/config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "Akarsh Tripathi | Space Portfolio",
+  title: "Akarsh Tripathi | Full Stack Developer",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
